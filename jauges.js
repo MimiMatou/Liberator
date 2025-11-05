187af95b269a79c1aacaacf2d4e2ad66edf1b952
Pascal BOURIT <pascal.bourit.e@thalesdigital.io>
2025-11-05 15:29:20 +0100

    refactor jauge 2 et 3

--- jauges.js
+++ jauges.js
@@ -1,7 +1,7 @@
 const jauges = [
     { nom: 'presentation', label: 'Présentation', niveaux: ["Entièrement habillée", "En tenue sexy", "Seins nus", "Entièrement nue", "Nue et dans des positions indécentes"] },
-    { nom: 'caresses', label: 'Caresses', niveaux: ["Caresser son pénis", "Caresser ses couilles", "Caresser son périnée", "Caresser son anus", "Doigter son anus"] },
-    { nom: 'exploration', label: 'Exploration', niveaux: ["Lécher son pénis", "Téter son gland", "Lécher ses couilles", "Lécher son périnée", "Lécher son anus"] },
+    { nom: 'caresses', label: 'Caresses', niveaux: ["Ne rien caresser", "Caresser son pénis", "Caresser ses couilles", "Caresser son anus", "Doigter son anus"] },
+    { nom: 'exploration', label: 'Exploration', niveaux: ["Ne rien lécher", "Lécher son pénis", "Téter son gland", "Lécher ses couilles", "Lécher son anus"] },
     { nom: 'oral', label: 'Oral', niveaux: ["Ne rien sucer", "Sucer son pénis", "Sucer ses couilles", "Sucer profondément, gorge profonde", "Te faire baiser la bouche"] },
     { nom: 'ejaculation', label: 'Éjaculation', niveaux: ["Éjaculation sur lui", "Éjaculation sur tes seins ou une autre partie de ton corps", "Éjaculation sur ou dans ton sexe", "Éjaculation dans ta bouche", "Éjaculation sur ton visage"] },
     { nom: 'sperme', label: 'Sperme', niveaux: ["Jouer, étaler, récolter le sperme avec le doigt", "Lécher le sperme", "Nettoyer le sperme avec la langue", "Avaler le sperme", "Partager le sperme"] }
